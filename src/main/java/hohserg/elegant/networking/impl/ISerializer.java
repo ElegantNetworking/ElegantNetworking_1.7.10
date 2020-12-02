bb50c4f9770f391aa3a0b5f1cdecc1795bcbf1e9
hohserg <hohserg1@yandex.ru>
2020-12-02 06:49:10 +0300

    Rename serializers methods: make more clear

--- src/main/java/hohserg/elegant/networking/impl/ISerializer.java
+++ src/main/java/hohserg/elegant/networking/impl/ISerializer.java
@@ -20,153 +20,153 @@ public interface ISerializer<Packet> {
 
     int packetId();
 
-    default void serializeBooleanGeneric(boolean value, ByteBuf acc) {
+    default void serialize_Boolean_Generic(boolean value, ByteBuf acc) {
         acc.writeBoolean(value);
     }
 
-    default void serializeByteGeneric(byte value, ByteBuf acc) {
+    default void serialize_Byte_Generic(byte value, ByteBuf acc) {
         acc.writeByte(value);
     }
 
-    default void serializeShortGeneric(short value, ByteBuf acc) {
+    default void serialize_Short_Generic(short value, ByteBuf acc) {
         acc.writeShort(value);
     }
 
-    default void serializeIntGeneric(int value, ByteBuf acc) {
+    default void serialize_Int_Generic(int value, ByteBuf acc) {
         acc.writeInt(value);
     }
 
-    default void serializeLongGeneric(long value, ByteBuf acc) {
+    default void serialize_Long_Generic(long value, ByteBuf acc) {
         acc.writeLong(value);
     }
 
-    default void serializeCharGeneric(char value, ByteBuf acc) {
+    default void serialize_Char_Generic(char value, ByteBuf acc) {
         acc.writeChar(value);
     }
 
-    default void serializeFloatGeneric(float value, ByteBuf acc) {
+    default void serialize_Float_Generic(float value, ByteBuf acc) {
         acc.writeFloat(value);
     }
 
-    default void serializeDoubleGeneric(double value, ByteBuf acc) {
+    default void serialize_Double_Generic(double value, ByteBuf acc) {
         acc.writeDouble(value);
     }
 
-    default void serializeStringGeneric(String value, ByteBuf acc) {
+    default void serialize_String_Generic(String value, ByteBuf acc) {
         ByteBufUtils.writeUTF8String(acc, value);
     }
 
-    default void serializeNBTTagCompoundGeneric(NBTTagCompound value, ByteBuf acc) {
+    default void serialize_NBTTagCompound_Generic(NBTTagCompound value, ByteBuf acc) {
         ByteBufUtils.writeTag(acc, value);
     }
 
-    default void serializeItemStackGeneric(ItemStack value, ByteBuf acc) {
+    default void serialize_ItemStack_Generic(ItemStack value, ByteBuf acc) {
         ByteBufUtils.writeItemStack(acc, value);
     }
 
-    default void serializeFluidStackGeneric(FluidStack value, ByteBuf acc) {
-        serializeFluidGeneric(value.getFluid(), acc);
+    default void serialize_FluidStack_Generic(FluidStack value, ByteBuf acc) {
+        serialize_Fluid_Generic(value.getFluid(), acc);
         acc.writeInt(value.amount);
         if (value.tag != null) {
             acc.writeByte(1);
-            serializeNBTTagCompoundGeneric(value.tag, acc);
+            serialize_NBTTagCompound_Generic(value.tag, acc);
         } else
             acc.writeByte(0);
     }
 
-    default void serializeItemGeneric(Item value, ByteBuf acc) {
+    default void serialize_Item_Generic(Item value, ByteBuf acc) {
         acc.writeShort(Item.getIdFromItem(value));
     }
 
-    default void serializeBlockGeneric(Block value, ByteBuf acc) {
+    default void serialize_Block_Generic(Block value, ByteBuf acc) {
         acc.writeShort(Block.getIdFromBlock(value));
     }
 
-    default void serializeFluidGeneric(Fluid value, ByteBuf acc) {
-        serializeStringGeneric(FluidRegistry.getFluidName(value), acc);
+    default void serialize_Fluid_Generic(Fluid value, ByteBuf acc) {
+        serialize_String_Generic(FluidRegistry.getFluidName(value), acc);
     }
 
-    default void serializeResourceLocationGeneric(ResourceLocation value, ByteBuf acc) {
-        serializeStringGeneric(value.toString(), acc);
+    default void serialize_ResourceLocation_Generic(ResourceLocation value, ByteBuf acc) {
+        serialize_String_Generic(value.toString(), acc);
     }
 
-    default void serializeUUIDGeneric(UUID value, ByteBuf acc) {
-        serializeStringGeneric(value.toString(), acc);
+    default void serialize_UUID_Generic(UUID value, ByteBuf acc) {
+        serialize_String_Generic(value.toString(), acc);
     }
 
 
-    default boolean unserializeBooleanGeneric(ByteBuf buf) {
+    default boolean unserialize_Boolean_Generic(ByteBuf buf) {
         return buf.readBoolean();
     }
 
-    default byte unserializeByteGeneric(ByteBuf buf) {
+    default byte unserialize_Byte_Generic(ByteBuf buf) {
         return buf.readByte();
     }
 
-    default short unserializeShortGeneric(ByteBuf buf) {
+    default short unserialize_Short_Generic(ByteBuf buf) {
         return buf.readShort();
     }
 
-    default int unserializeIntGeneric(ByteBuf buf) {
+    default int unserialize_Int_Generic(ByteBuf buf) {
         return buf.readInt();
     }
 
-    default long unserializeLongGeneric(ByteBuf buf) {
+    default long unserialize_Long_Generic(ByteBuf buf) {
         return buf.readLong();
     }
 
-    default char unserializeCharGeneric(ByteBuf buf) {
+    default char unserialize_Char_Generic(ByteBuf buf) {
         return buf.readChar();
     }
 
-    default float unserializeFloatGeneric(ByteBuf buf) {
+    default float unserialize_Float_Generic(ByteBuf buf) {
         return buf.readFloat();
     }
 
-    default double unserializeDoubleGeneric(ByteBuf buf) {
+    default double unserialize_Double_Generic(ByteBuf buf) {
         return buf.readDouble();
     }
 
-    default String unserializeStringGeneric(ByteBuf buf) {
+    default String unserialize_String_Generic(ByteBuf buf) {
         return ByteBufUtils.readUTF8String(buf);
     }
 
-    default NBTTagCompound unserializeNBTTagCompoundGeneric(ByteBuf buf) {
+    default NBTTagCompound unserialize_NBTTagCompound_Generic(ByteBuf buf) {
         return ByteBufUtils.readTag(buf);
     }
 
-    default ItemStack unserializeItemStackGeneric(ByteBuf buf) {
+    default ItemStack unserialize_ItemStack_Generic(ByteBuf buf) {
         return ByteBufUtils.readItemStack(buf);
     }
 
-    default FluidStack unserializeFluidStackGeneric(ByteBuf buf) {
-        Fluid fluid = unserializeFluidGeneric(buf);
+    default FluidStack unserialize_FluidStack_Generic(ByteBuf buf) {
+        Fluid fluid = unserialize_Fluid_Generic(buf);
         if (fluid != null) {
             FluidStack stack = new FluidStack(fluid, buf.readInt());
             if (buf.readByte() == 1)
-                stack.tag = unserializeNBTTagCompoundGeneric(buf);
+                stack.tag = unserialize_NBTTagCompound_Generic(buf);
             return stack;
         } else
             return null;
     }
 
-    default Item unserializeItemGeneric(ByteBuf buf) {
+    default Item unserialize_Item_Generic(ByteBuf buf) {
         return Item.getItemById(buf.readShort());
     }
 
-    default Block unserializeBlockGeneric(ByteBuf buf) {
+    default Block unserialize_Block_Generic(ByteBuf buf) {
         return Block.getBlockById(buf.readShort());
     }
 
-    default Fluid unserializeFluidGeneric(ByteBuf buf) {
-        return FluidRegistry.getFluid(unserializeStringGeneric(buf));
+    default Fluid unserialize_Fluid_Generic(ByteBuf buf) {
+        return FluidRegistry.getFluid(unserialize_String_Generic(buf));
     }
 
-    default ResourceLocation unserializeResourceLocationGeneric(ByteBuf buf) {
-        return new ResourceLocation(unserializeStringGeneric(buf));
+    default ResourceLocation unserialize_ResourceLocation_Generic(ByteBuf buf) {
+        return new ResourceLocation(unserialize_String_Generic(buf));
     }
 
-    default UUID unserializeUUIDGeneric(ByteBuf buf) {
-        return UUID.fromString(unserializeStringGeneric(buf));
+    default UUID unserialize_UUID_Generic(ByteBuf buf) {
+        return UUID.fromString(unserialize_String_Generic(buf));
     }
 }
