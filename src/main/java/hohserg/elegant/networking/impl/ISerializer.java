56b3dbc6822e7171c207070173f9c52cde8bf51b
hohserg <hohserg1@yandex.ru>
2020-12-02 06:46:47 +0300

    Added serialization support for NBT, ItemStack, FluidStack, Item, Block, Fluid, ResourceLocation, UUID
    Added serialization support for NBT, ItemStack, FluidStack, Item, Block, Fluid, ResourceLocation, UUID

--- src/main/java/hohserg/elegant/networking/impl/ISerializer.java
+++ src/main/java/hohserg/elegant/networking/impl/ISerializer.java
@@ -2,6 +2,16 @@ package hohserg.elegant.networking.impl;
 
 import cpw.mods.fml.common.network.ByteBufUtils;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+
+import java.util.UUID;
 
 public interface ISerializer<Packet> {
     void serialize(Packet value, ByteBuf acc);
@@ -46,6 +56,44 @@ public interface ISerializer<Packet> {
         ByteBufUtils.writeUTF8String(acc, value);
     }
 
+    default void serializeNBTTagCompoundGeneric(NBTTagCompound value, ByteBuf acc) {
+        ByteBufUtils.writeTag(acc, value);
+    }
+
+    default void serializeItemStackGeneric(ItemStack value, ByteBuf acc) {
+        ByteBufUtils.writeItemStack(acc, value);
+    }
+
+    default void serializeFluidStackGeneric(FluidStack value, ByteBuf acc) {
+        serializeFluidGeneric(value.getFluid(), acc);
+        acc.writeInt(value.amount);
+        if (value.tag != null) {
+            acc.writeByte(1);
+            serializeNBTTagCompoundGeneric(value.tag, acc);
+        } else
+            acc.writeByte(0);
+    }
+
+    default void serializeItemGeneric(Item value, ByteBuf acc) {
+        acc.writeShort(Item.getIdFromItem(value));
+    }
+
+    default void serializeBlockGeneric(Block value, ByteBuf acc) {
+        acc.writeShort(Block.getIdFromBlock(value));
+    }
+
+    default void serializeFluidGeneric(Fluid value, ByteBuf acc) {
+        serializeStringGeneric(FluidRegistry.getFluidName(value), acc);
+    }
+
+    default void serializeResourceLocationGeneric(ResourceLocation value, ByteBuf acc) {
+        serializeStringGeneric(value.toString(), acc);
+    }
+
+    default void serializeUUIDGeneric(UUID value, ByteBuf acc) {
+        serializeStringGeneric(value.toString(), acc);
+    }
+
 
     default boolean unserializeBooleanGeneric(ByteBuf buf) {
         return buf.readBoolean();
@@ -81,5 +129,44 @@ public interface ISerializer<Packet> {
 
     default String unserializeStringGeneric(ByteBuf buf) {
         return ByteBufUtils.readUTF8String(buf);
+    }
+
+    default NBTTagCompound unserializeNBTTagCompoundGeneric(ByteBuf buf) {
+        return ByteBufUtils.readTag(buf);
+    }
+
+    default ItemStack unserializeItemStackGeneric(ByteBuf buf) {
+        return ByteBufUtils.readItemStack(buf);
+    }
+
+    default FluidStack unserializeFluidStackGeneric(ByteBuf buf) {
+        Fluid fluid = unserializeFluidGeneric(buf);
+        if (fluid != null) {
+            FluidStack stack = new FluidStack(fluid, buf.readInt());
+            if (buf.readByte() == 1)
+                stack.tag = unserializeNBTTagCompoundGeneric(buf);
+            return stack;
+        } else
+            return null;
+    }
+
+    default Item unserializeItemGeneric(ByteBuf buf) {
+        return Item.getItemById(buf.readShort());
+    }
+
+    default Block unserializeBlockGeneric(ByteBuf buf) {
+        return Block.getBlockById(buf.readShort());
+    }
+
+    default Fluid unserializeFluidGeneric(ByteBuf buf) {
+        return FluidRegistry.getFluid(unserializeStringGeneric(buf));
+    }
+
+    default ResourceLocation unserializeResourceLocationGeneric(ByteBuf buf) {
+        return new ResourceLocation(unserializeStringGeneric(buf));
+    }
+
+    default UUID unserializeUUIDGeneric(ByteBuf buf) {
+        return UUID.fromString(unserializeStringGeneric(buf));
     }
 }
