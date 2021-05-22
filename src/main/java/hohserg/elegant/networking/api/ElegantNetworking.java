5a6e141c58046c7bf356cf4133a7cbb5cfb45ee5
hohserg <hohserg1@yandex.ru>
2021-05-22 19:47:30 +0300

    Up version. Added javadoc

--- src/main/java/hohserg/elegant/networking/api/ElegantNetworking.java
+++ src/main/java/hohserg/elegant/networking/api/ElegantNetworking.java
@@ -4,6 +4,17 @@ import hohserg.elegant.networking.impl.ISerializerBase;
 import hohserg.elegant.networking.impl.Registry;
 
 public class ElegantNetworking {
+    /**
+     * Gives a serializer for requested type.
+     * Serializer generally is pair of functions:
+     *
+     * {@code A => ByteBuf }
+     * {@code ByteBuf => A }
+     *
+     * @param <A>          requested type
+     * @param serializable class of requested type
+     * @return serializer
+     */
     public static <A extends IByteBufSerializable> ISerializerBase<A> getByteBufSerializerFor(Class<A> serializable) {
         return Registry.getSerializerFor(serializable);
     }
